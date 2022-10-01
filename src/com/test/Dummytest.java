5c78d48531fe87dbafad39e4e8bec7500403738b
Devansp <deva.aaru@gmail.com>
2022-10-01 23:17:59 +0530

    first commit "change in Dummytest".

---
+++ src/com/test/Dummytest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Dummytest {
+
+	private static void add() {
+   System.out.println(" master: Git check");
+	}
+	
+	public static void main(String[] args) {
+		add();
+	}
+}
